7728bbe7d9bb9f88d37a5e06a21a0a0ea51037f3
Fajar Siddiqi Putu Saed <47381662+fajardiki@users.noreply.github.com>
2019-05-13 09:44:18 +0700

    ngapload

--- app/src/main/java/com/example/projectandroidedesa/Home.java
+++ app/src/main/java/com/example/projectandroidedesa/Home.java
@@ -1,10 +1,14 @@
 package com.example.projectandroidedesa;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 public class Home extends AppCompatActivity {
 
@@ -12,21 +16,8 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
     }
 
-    public boolean onCreateOptionMenu(Home menu){
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.optionmenu, (android.view.Menu) menu);
-        return true;
-    }
-
-    public boolean onOptionItemSelected(MenuItem item){
-        if (item.getItemId()==R.id.home){
-            startActivity(new Intent(this, Home.class));
-        } else if (item.getItemId()==R.id.pengajuan){
-            startActivity(new Intent(this, Pengajuan.class));
-        }
-
-        return true;
-    }
 }
