1b9691f2afc74b150e39c678c81f9d3f1060679e
Fajar Siddiqi Putu Saed <47381662+fajardiki@users.noreply.github.com>
2019-05-14 00:58:42 +0700

    halaman home, pengajuan dan pengaturan

--- app/src/main/java/com/example/projectandroidedesa/MainActivity.java
+++ app/src/main/java/com/example/projectandroidedesa/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.projectandroidedesa;
 
-import android.app.Fragment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -30,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         pengajuanFragment = new PengajuanFragment();
         pengaturanFragment = new PengaturanFragment();
 
+        setFragment(homeFragment);
+
         mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -41,19 +44,26 @@ public class MainActivity extends AppCompatActivity {
                         return true;
                     case R.id.nav_pengajuan:
                         mMainNav.setItemBackgroundResource(R.color.colorPrimary);
+                        setFragment(pengajuanFragment);
                         return true;
                     case R.id.nav_pengaturan:
                         mMainNav.setItemBackgroundResource(R.color.colorAccent);
+                        setFragment(pengaturanFragment);
                         return true;
                         default:
                             return false;
                 }
             }
 
-            private void setFragment(Fragment fragment) {
-                
-            }
         });
+    }
+
+    private void setFragment(Fragment fragment) {
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
+
     }
 
 }
