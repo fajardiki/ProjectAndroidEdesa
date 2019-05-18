c66ec633707901e6059cb7d009df5e23d09c0df0
Fajar Siddiqi Putu Saed <47381662+fajardiki@users.noreply.github.com>
2019-05-18 21:16:57 +0700

    list view fragment home

--- app/src/main/java/com/example/projectandroidedesa/PengajuanFragment.java
+++ app/src/main/java/com/example/projectandroidedesa/PengajuanFragment.java
@@ -6,13 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PengajuanFragment extends Fragment {
-
+    ListView lv;
 
     public PengajuanFragment() {
         // Required empty public constructor
