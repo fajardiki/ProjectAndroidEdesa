99ac8c074635caafd36d14f4ba998cad19c72de2
Fajar Siddiqi Putu Saed <47381662+fajardiki@users.noreply.github.com>
2019-05-21 21:45:10 +0700

    akubuathomelagi

--- app/src/main/java/com/example/projectandroidedesa/HomeFragment.java
+++ app/src/main/java/com/example/projectandroidedesa/HomeFragment.java
@@ -1,12 +1,26 @@
 package com.example.projectandroidedesa;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import org.json.JSONObject;
 
+import java.util.HashMap;
+
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,14 +32,79 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private ListView listView;
+
+    private String JSON_STRING;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        listView = (ListView) rootView.findViewById(R.id.listView);
+        getJSON();
 
+        return rootView;
+    }
+
+    private void showEmployee(){
+        JSONObject jsonObject = null;
+        ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String, String>>();
+        try {
+            jsonObject = new JSONObject(JSON_STRING);
+            JSONArray result = jsonObject.getJSONArray(konfigurasi.TAG_JSON_ARRAY);
+
+            for(int i = 0; i<result.length(); i++){
+                JSONObject jo = result.getJSONObject(i);
+                String id = jo.getString(konfigurasi.TAG_ID);
+                String name = jo.getString(konfigurasi.TAG_NAMA);
+
+                HashMap<String,String> employees = new HashMap<>();
+                employees.put(konfigurasi.TAG_ID,id);
+                employees.put(konfigurasi.TAG_NAMA,name);
+                list.add(employees);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        ListAdapter adapter = new SimpleAdapter(
+                getActivity(), list, R.layout.list_pengumuman,
+                new String[]{konfigurasi.TAG_ID,konfigurasi.TAG_NAMA},
+                new int[]{R.id.id, R.id.name});
+
+        listView.setAdapter(adapter);
+    }
+
+    private void getJSON(){
+        class GetJSON extends AsyncTask<Void,Void, String> {
+
+            private ProgressDialog loading;
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(getActivity(),"Mengambil Data","Mohon Tunggu...",false,false);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                JSON_STRING = s;
+                showEmployee();
+            }
+
+            @Override
+            protected String doInBackground(Void... params) {
+                RequestHandler rh = new RequestHandler();
+                String s = rh.sendGetRequest(konfigurasi.URL_GET_ALL);
+                return s;
+            }
+        }
+        GetJSON gj = new GetJSON();
+        gj.execute();
     }
 
 }
